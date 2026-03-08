b1fb6d47078d5d425a0c1ed4c0861aa59932deb4
AmbiMood <ambimood2026@gmail.com>
2026-03-08 20:55:43 +0530

    fix: auth route path

--- server.js
+++ server.js
@@ -317,7 +317,7 @@ app.use(express.json());
 const authRoutes = require('./routes/authuRoutes');
 const musicRoutes = require('./routes/musicRoutes');
 
-app.use('/api', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/music', musicRoutes);
 
 app.get('/', (req, res) => {
